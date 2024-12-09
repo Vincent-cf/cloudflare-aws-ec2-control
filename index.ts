41c6d049a20ba33deeccff4401dedab0270c9791
Vincent Barida <vbarida@cloudflare.com>
2024-12-09 19:03:51 +0100

    Initial commit

--- index.ts
+++ index.ts
@@ -3,19 +3,19 @@ import { EC2 } from "@aws-sdk/client-ec2";
 interface Env {
   AWS_ACCESS_KEY: string;
   AWS_SECRET_KEY: string;
+  INSTANCE_ID: string;
 }
 
 export default {
   async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext): Promise<void> {
     const awsAccessKey = env.AWS_ACCESS_KEY;
     const awsSecretKey = env.AWS_SECRET_KEY;
+    const instanceId = env.INSTANCE_ID;
     const region = "eu-west-1"; // Hardcoded region
 
     if (!awsAccessKey || !awsSecretKey) {
       return;
     }
-
-    const instanceId = "i-01aa59cc2a32995b8"; 
 
     const ec2Client = new EC2({
       region: region,
